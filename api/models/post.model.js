44846702f7c7275bb7ef5dcf03625c2305236404
martins091 <agbomartinsejiofor@gmail.com>
2024-02-04 12:45:20 +0100

    add create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://img.freepik.com/free-photo/technology-communication-icons-symbols-concept_53876-120314.jpg'
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
