256d290a85661905524c3f023f4829e6089ddee8
martins091 <agbomartinsejiofor@gmail.com>
2024-02-17 15:27:31 +0100

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.rout.js'
 import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -29,6 +30,7 @@ console.log('Sever is running on port 3000!')
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
